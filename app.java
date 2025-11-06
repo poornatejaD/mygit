b7b4c14b76eb9a0e2a7dcf8b2c5b8cccc4d1cde1
poornatejaD <poornatejadakavarapu16@gmail.com>
2025-11-06 16:56:02 +0530

    new code is added without pull

--- app.java
+++ app.java
@@ -5,3 +5,16 @@ public class HelloWorld {
     }
 }
 
+public class QuotientRemainder {
+
+  public static void main(String[] args) {
+
+    int dividend = 25, divisor = 4;
+
+    int quotient = dividend / divisor;
+    int remainder = dividend % divisor;
+
+    System.out.println("Quotient = " + quotient);
+    System.out.println("Remainder = " + remainder);
+  }
+}
